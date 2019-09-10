0873faf710354b51045b70ab4679013045749bad
Erick <erick.lauridsen@gmail.com>
2019-09-10 11:37:47 +0200

    Automation in maven

---
+++ src/main/webapp/precache-manifest.d3f2a1a05b1e5446f936279bc6361ab1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9410a9f2df7e255c228ed25e8ef45428",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b131d373c84c4d3f54ea",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e1d9cc27791ac614d15",
+    "url": "/static/js/2.8ce7f97d.chunk.js"
+  },
+  {
+    "revision": "b131d373c84c4d3f54ea",
+    "url": "/static/js/main.867aa8c0.chunk.js"
+  },
+  {
+    "revision": "faf3d913dea1519336c1",
+    "url": "/static/js/runtime~main.0b761a9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
