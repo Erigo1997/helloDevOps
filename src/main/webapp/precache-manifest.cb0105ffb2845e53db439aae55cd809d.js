98db0da273b94969df015482658039bf90b3a2b0
Erick <erick.lauridsen@gmail.com>
2019-09-17 09:49:42 +0200

    Email input

---
+++ src/main/webapp/precache-manifest.cb0105ffb2845e53db439aae55cd809d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697480fb924acf867700b0ed23f21df9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "375f2563f989bcf860eb",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1a2a8bd8d9751e1d7a95",
+    "url": "/static/js/2.a0f03215.chunk.js"
+  },
+  {
+    "revision": "375f2563f989bcf860eb",
+    "url": "/static/js/main.916f54d4.chunk.js"
+  },
+  {
+    "revision": "faf3d913dea1519336c1",
+    "url": "/static/js/runtime~main.0b761a9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ web/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Form from 'react-bootstrap/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. However, javascript is a nightmare.
+        </p>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else. Maybe with Zuckerberg. But not with Gates.
+              We'll never share your email with Bill Gates.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicChecbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+      </Form>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
